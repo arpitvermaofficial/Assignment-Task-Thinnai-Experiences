222690ae1d17d37950e7bbf0f7cc6d9959a22a6a
Arpit Verma <99536186+arpitvermaofficial@users.noreply.github.com>
2023-02-06 20:07:04 +0530

    Update widgets.dart
    Updated

--- lib/components/widgets.dart
+++ lib/components/widgets.dart
@@ -6,31 +6,44 @@ import '../Screens/screen2.dart';
 Color color1 = Color(0xff9e9b9c);
 Color categorycard2clr = Colors.white;
 
-class categorycard extends StatelessWidget {
+class categorycard extends StatefulWidget {
   const categorycard({Key? key, required this.imagename, required this.text})
       : super(key: key);
   final String imagename;
   final String text;
+
+  @override
+  State<categorycard> createState() => _categorycardState();
+}
+bool selected=false;
+class _categorycardState extends State<categorycard> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Container(
-      height: size.height * 0.06,
-      padding: EdgeInsets.fromLTRB(30, 10, 30, 10),
-      decoration: BoxDecoration(
-        color: Colors.transparent,
-        border: Border.all(color: Color(0xffd9d9d9)),
-        borderRadius: BorderRadius.all(Radius.circular(15)),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [Image.asset('assets/$imagename'), Text("$text")],
+    return GestureDetector(
+      onTap: (){
+        setState(() {
+          selected=!selected;
+        });
+      },
+      child: Container(
+        height: size.height * 0.06,
+        padding: EdgeInsets.fromLTRB(30, 10, 30, 10),
+        decoration: BoxDecoration(
+          color: Colors.transparent,
+          border: Border.all(color:selected?Colors.green:Color(0xffd9d9d9)),
+          borderRadius: BorderRadius.all(Radius.circular(15)),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [Image.asset('assets/${widget.imagename}'), Text("${widget.text}")],
+        ),
       ),
     );
   }
 }
 
-class Categorycard2 extends StatelessWidget {
+class Categorycard2 extends StatefulWidget {
   const Categorycard2(
       {Key? key,
       required this.place,
@@ -44,6 +57,12 @@ class Categorycard2 extends StatelessWidget {
   final double rating;
   final String image;
   final int price;
+
+  @override
+  State<Categorycard2> createState() => _Categorycard2State();
+}
+bool selectedcard2 =false;
+class _Categorycard2State extends State<Categorycard2> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -55,16 +74,16 @@ class Categorycard2 extends StatelessWidget {
                 context,
                 MaterialPageRoute(
                     builder: (context) => screen2(
-                        place: place,
-                        location: location,
-                        image: image,
-                        rating: rating,
-                        price: price)));
+                        place: widget.place,
+                        location: widget.location,
+                        image: widget.image,
+                        rating: widget.rating,
+                        price: widget.price)));
           },
           child: Container(
             decoration: BoxDecoration(
               image: DecorationImage(
-                  fit: BoxFit.fill, image: AssetImage("assets/$image")),
+                  fit: BoxFit.fill, image: AssetImage("assets/${widget.image}")),
               borderRadius: BorderRadius.all(
                 Radius.circular(15),
               ),
@@ -81,11 +100,15 @@ class Categorycard2 extends StatelessWidget {
               backgroundColor: MaterialStateProperty.all(categorycard2clr),
               shape: MaterialStateProperty.all(CircleBorder()),
             ),
-            onPressed: () {},
+            onPressed: () {
+              setState(() {
+                selectedcard2=!selectedcard2;
+              });
+            },
             child: Icon(
               size: 19,
               Icons.favorite,
-              color: Colors.red,
+              color: selectedcard2?Colors.red:Colors.grey,
             ),
           ),
         ),
@@ -93,7 +116,7 @@ class Categorycard2 extends StatelessWidget {
             left: 10,
             bottom: 50,
             child: Text(
-              "$place",
+              "${widget.place}",
               style: TextStyle(
                   color: categorycard2clr, fontWeight: FontWeight.bold),
             )),
@@ -108,7 +131,7 @@ class Categorycard2 extends StatelessWidget {
                 color: Colors.white,
               ),
               Text(
-                "$location",
+                "${widget.location}",
                 style: TextStyle(
                   fontSize: 10,
                   color: categorycard2clr,
@@ -121,7 +144,7 @@ class Categorycard2 extends StatelessWidget {
             bottom: 10,
             left: 10,
             child: Ratingstar(
-              rating: rating,
+              rating: widget.rating,
               ratingcolor: categorycard2clr,
               ratingsize: 12.0,
             ))
@@ -181,7 +204,7 @@ class Ratingstar extends StatelessWidget {
   }
 }
 
-class Packagecard extends StatelessWidget {
+class Packagecard extends StatefulWidget {
   const Packagecard(
       {Key? key,
       required this.rating,
@@ -193,72 +216,97 @@ class Packagecard extends StatelessWidget {
   final int price;
   final double rating;
   final String place;
+
+  @override
+  State<Packagecard> createState() => _PackagecardState();
+}
+bool slected=false;
+
+class _PackagecardState extends State<Packagecard> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Container(
-      height: size.height * 0.16,
-      padding: EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        border: Border.all(color: color1.withOpacity(1)),
-        borderRadius: BorderRadius.all(
-          Radius.circular(15),
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => screen2(
+                    place: widget.place,
+                    location: "",
+                    image: widget.image,
+                    rating: widget.rating,
+                    price: widget.price)));
+      },
+      child: Container(
+        height: size.height * 0.16,
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          border: Border.all(color: color1.withOpacity(1)),
+          borderRadius: BorderRadius.all(
+            Radius.circular(15),
+          ),
         ),
-      ),
-      child: Row(
-        children: [
-          Expanded(
-            flex: 1,
-            child: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    fit: BoxFit.fill, image: AssetImage("assets/$image")),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(15),
+        child: Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.fill, image: AssetImage("assets/${widget.image}")),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(15),
+                  ),
                 ),
               ),
             ),
-          ),
-          SizedBox(
-            width: size.width * 0.02,
-          ),
-          Expanded(
-            flex: 3,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "$place",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 13),
-                    ),
-                    Icon(Icons.favorite_border)
-                  ],
-                ),
-                Text(
-                  "₹$price",
-                  style: TextStyle(fontSize: 13, color: Colors.red),
-                ),
-                Ratingstar(
-                  rating: rating,
-                  ratingcolor: Colors.black,
-                  ratingsize: 18,
-                ),
-                Text(
-                  "A resort is a place used for vacation, relaxation or as a day...",
-                  style: TextStyle(
-                    color: color1.withOpacity(1),
-                    fontSize: 12,
-                  ),
-                ),
-              ],
+            SizedBox(
+              width: size.width * 0.02,
             ),
-          )
-        ],
+            Expanded(
+              flex: 3,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "${widget.place}",
+                        style:
+                            TextStyle(fontWeight: FontWeight.bold, fontSize: 13),
+                      ),
+                      GestureDetector(onTap: (){
+                        setState(() {
+                          selected=!selected;
+                        });
+                      },
+                          child: Icon(Icons.favorite_border,color: selected?Colors.red:Colors.black,))
+                    ],
+                  ),
+                  Text(
+                    "₹${widget.price}",
+                    style: TextStyle(fontSize: 13, color: Colors.red),
+                  ),
+                  Ratingstar(
+                    rating: widget.rating,
+                    ratingcolor: Colors.black,
+                    ratingsize: 18,
+                  ),
+                  Text(
+                    "A resort is a place used for vacation, relaxation or as a day...",
+                    style: TextStyle(
+                      color: color1.withOpacity(1),
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
