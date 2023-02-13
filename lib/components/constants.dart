c401a1b3797985351c5c3072dee14119f6779d46
Arpit Verma <99536186+arpitvermaofficial@users.noreply.github.com>
2023-02-13 21:55:24 +0530

    Update constants.dart

--- lib/components/constants.dart
+++ lib/components/constants.dart
@@ -7,3 +7,8 @@ OutlineInputBorder textfieldborder = OutlineInputBorder(
     10,
   )),
 );
+List <String> place=["Kuta Beach","Baga Beach","Kuta Beach","Baga Beach"];
+List <String> location=["Bali ,Indonasia","Goa India","Bali ,Indonasia","Goa India"];
+List <String> image=["Bali.png","Baga.png","Bali.png","Baga.png"];
+List <double> rating=[4.2,4.2,4.2,4.2];
+List <int> price=[15000,15000,15000,15000];
